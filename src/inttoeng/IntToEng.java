38225c184c72d69aedfb500650f4b9e1b332a023
miyahana <g13923mh@gm.tsuda.ac.jp>
2015-05-20 20:30:22 +0900

    0から10000までの数字を出力できるように変更しました。

--- src/inttoeng/IntToEng.java
+++ src/inttoeng/IntToEng.java
@@ -46,8 +46,26 @@ public class IntToEng {
     	String[] eNum7={"ten", "eleven", "twelve", "thirteen", "fourteen",
 		"fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
     	
+    	//1000^9999までの1000の位
+    	String[] eNum8={"onethousand","twothousand","threethousand","fourthousand",
+    			"fivethousand","sixthousand","seventhousand","eighthousand","ninethousand"};
     	
-    			
+    	//1000^9999までの100の位
+    	String[] eNum9={"onehoundred","twohoundred","threehoundred","fourhandred",
+    			"fivehoundred","sixhoundred","sevenhoundred","eighthoundred","ninehoundred"};
+    	
+    	//1000^9999までの10の位
+    	String[] eNum10={"","twenty", "thirty", "forty", "fifty", "sixty",
+    			"seventy", "eighty", "ninety"};
+    	
+    	//1000^9999までの1の位
+    	String[] eNum11={"", "one", "two", "three", "four",
+    			"five", "six", "seven", "eight", "nine"};
+    	
+    	//1000^9999までの1の位	
+    	String[] eNum12={"ten", "eleven", "twelve", "thirteen", "fourteen",
+    			"fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+    		
     	String num = String.valueOf(n);
     	if(n < 20){ 
     		num=eNum1[n];
@@ -102,6 +120,66 @@ public class IntToEng {
     		num=eNum4[num100b-1]+eNum5[num10c-1]+eNum6[num1c];
     			return num;
     	}
-        return "";
+    	if(n>=1000 && n<=1009){
+    		String num1000 = num.substring(0,1);
+    		String num100b = num.substring(1,2);
+    		String num10b = num.substring(2,3);
+    		String num1b = num.substring(3,4);
+    		
+    		
+    		int num1000b = Integer.parseInt(num1000);
+    		int num100c = Integer.parseInt(num100b);
+    		int num10c = Integer.parseInt(num10b);
+    		int num1c = Integer.parseInt(num1b);
+    		
+    		num=eNum8[num1000b-1]+eNum10[num10c]+eNum11[num1c];
+    			return num;
+    	}
+    	if(n>=1010 && n<=1019){
+    		String num1000 = num.substring(0,1);
+    		String num100b = num.substring(1,2);
+    		String num10b = num.substring(2,3);
+    		String num1b = num.substring(3,4);
+    		
+    		
+    		int num1000b = Integer.parseInt(num1000);
+    		int num100c = Integer.parseInt(num100b);
+    		int num10c = Integer.parseInt(num10b);
+    		int num1c = Integer.parseInt(num1b);
+    		
+    		num=eNum8[num1000b-1]+eNum10[num10c-1]+eNum12[num1c];
+    			return num;
+    	}
+    	if(n>=1020 && n<=1099){
+    		String num1000 = num.substring(0,1);
+    		String num100b = num.substring(1,2);
+    		String num10b = num.substring(2,3);
+    		String num1b = num.substring(3,4);
+    		
+    		
+    		int num1000b = Integer.parseInt(num1000);
+    		int num100c = Integer.parseInt(num100b);
+    		int num10c = Integer.parseInt(num10b);
+    		int num1c = Integer.parseInt(num1b);
+    		
+    		num=eNum8[num1000b-1]+eNum10[num10c-1]+eNum11[num1c];
+    			return num;
+    	}
+    	if(n>=1100 && n<=9999){
+    		String num1000 = num.substring(0,1);
+    		String num100b = num.substring(1,2);
+    		String num10b = num.substring(2,3);
+    		String num1b = num.substring(3,4);
+    		
+    		
+    		int num1000b = Integer.parseInt(num1000);
+    		int num100c = Integer.parseInt(num100b);
+    		int num10c = Integer.parseInt(num10b);
+    		int num1c = Integer.parseInt(num1b);
+    		
+    		num=eNum8[num1000b-1]+eNum9[num100c-1]+eNum10[num10c-1]+eNum11[num1c];
+    			return num;
+    	}
+        return "tenthousand";
     }
 }
